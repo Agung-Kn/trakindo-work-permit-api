a1bc381ddb5acc05edd688fa5c81045bf62c92cd
Agung-Kn <agung.kn21@gmail.com>
2025-09-12 14:52:03 +0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,23 +1,30 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import router from './routes/router.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import routes from "./routes/index.js";
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(json());
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : [];
 
-app.use('/api/v1/', router);
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed CORS"));
+    }
+  },
+  credentials: true,
+}));
 
-app.get('/', (req, res) => {
-	res.send({ message: 'API is running' });
-});
+app.use(cookieParser());
+app.use(express.json());
 
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({
-		message: 'Internal Server Error',
-		error: err.message,
-	});
-});
+// Routes
+app.use();
+app.use(routes);
 
 export default app;
